18bcbf8fb25953b6d737e3ea65fe4d9319071cd0
Your Name <you@example.com>
2023-09-19 16:57:47 +0500

    latest commit

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get('/helloAhmad', (req, res) => {
   res.status(200).send("Hello, Ahmad!").end();
 });
 
+app.get('/posts', (req, res) => {
+  try {
+    res.status(200).send("Here are all the posts!").end(); 
+  } catch(error) {
+    res.status(500).send(`Some error occured ${error}`).end();
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
